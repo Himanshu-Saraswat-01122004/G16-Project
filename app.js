47b30d36e2a2f9efe4a955274cbc36dab776bd50
Himanshu-Saraswat-01122004 <himanshu.s22@iiits.in>
2024-04-03 23:51:04 +0530

    remove node modules

--- app.js
+++ app.js
@@ -4,16 +4,21 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const path = require('path');
 const html = require('html');
-const { error } = require('console');
-
-const app = express();
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 const notifier = require('node-notifier');
 
+const app = express();
 app.set('views', __dirname + '/views');
-app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(express.static(path.join(__dirname, 'public'))); 
+
 app.set('view engine', 'ejs');
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+
+
 mongoose.connect('mongodb://localhost:27017/G-16', {
     useNewUrlParser: true,
     useUnifiedTopology: true
@@ -25,40 +30,43 @@ mongoose.connect('mongodb://localhost:27017/G-16', {
         console.log('Error: ', err.message);
     });
 
-
-const newuser = new mongoose.Schema({
-    name: String,
-    username: String,
-    password: String
-});
-const User = mongoose.model('User', newuser);
-app.get('/', (req, res) => {
-    res.render('Home.ejs');
-});
-app.get('/home', (req, res) => {
-    res.render('Home.ejs');
-});
-app.get('/login', (req, res) => {
-    res.render('login.ejs');
-});
-app.get('/dashboard', (req, res) => {
-    res.render('dashboard.ejs');
-});
-app.get('/forex', (req, res) => {
-    res.render('forex.ejs');
-});
-app.get('/pricing', (req, res) => {
-    res.render('pricing.ejs');
-});
-// Define routes
-app.get('/about', (req, res) => {
-    res.render('about.ejs'); // Render the About page
-});
+    
+    const newuser = new mongoose.Schema({
+        name: String,
+        username: String,
+        password: String
+    });
+    const User = mongoose.model('User', newuser);
+    
+    app.get('/', (req, res) => {
+        res.render('Home.ejs');
+    });
+    app.get('/home', (req, res) => {
+        res.render('Home.ejs');
+    });
+    app.get('/login', (req, res) => {
+        res.render('login.ejs');
+    });
+    app.get('/dashboard', (req, res) => {
+        res.render('dashboard.ejs');
+    });
+    app.get('/forex', (req, res) => {
+        res.render('forex.ejs');
+    });
+    app.get('/pricing', (req, res) => {
+        res.render('pricing.ejs');
+    });
+    app.get('/about', (req, res) => {
+        res.render('about.ejs'); // Render the About page
+    });
+const SaltRounds = bcrypt.genSaltSync(10);
 app.post('/signup', async (req, res) => {
+    const hash = bcrypt.hashSync(req.body.password, SaltRounds);
+
     const user = new User({
         name: req.body.name,
         username: req.body.username,
-        password: req.body.password
+        password: hash,
     });
     await user.save()
         .then(() => {
@@ -73,7 +81,8 @@ app.post('/signup', async (req, res) => {
 app.post('/signin', async (req, res) => {
     const username = req.body.username;
     const password = req.body.password;
-    const user = await User.findOne({ username: username, password: password });
+    const hash = bcrypt.hashSync(password, SaltRounds);
+    const user = await User.findOne({ username: username, password: hash });
     if (user) {
         res.redirect('/dashboard');
     } else {
